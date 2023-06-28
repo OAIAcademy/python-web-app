41f3c84d6a2b6117b9d07a35c5cae83ddbda9190
torla <munford.ns@gmail.com>
2023-06-28 15:27:16 +0200

    new endpoint

--- data_access/query.py
+++ data_access/query.py
@@ -11,11 +11,22 @@ def get_connection():
         database=os.environ.get("DB_NAME"))
 
 
-def get_country_data(country_code: str,year:int):
+def get_country_data(country_code: str, year: int):
     with get_connection() as conn:
         return pd.read_sql('''select i.indicator_code,r.indicator_value,i.short_definition,i.long_definition
                                 from records as r
                                 join indicators i on r.indicator_code = i.indicator_code
                                 where country_code = %(country_code)s and r.year = %(year)s
                                 order by r.year ''',
-                           conn, params={'country_code': country_code,"year":year})
+                           conn, params={'country_code': country_code, "year": year})
+
+
+def get_indicator_average(indicator_code: str, year: int):
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute('''select r.indicator_code, avg(r.indicator_value)
+                                  from records as r
+                                  where r.indicator_code = %(country_code)s and year = %(year)s
+                                  group by r.indicator_code''',
+                           {'country_code': indicator_code, "year": year})
+            return str(cursor.fetchone()[1])
--- service/endpoints.py
+++ service/endpoints.py
@@ -1,12 +1,17 @@
 import flask
 from flask import Flask, Response
-from data_access.query import get_country_data
+from data_access.query import get_country_data, get_indicator_average
 
 app = Flask(__name__)
 
 
 @app.get('/country_data/<country_code>/<year>')  # defining a get endpoint
-def hello(country_code, year):
+def country(country_code, year):
     return flask.Response(content_type='application/json',
                           response=get_country_data(country_code,
                                                     int(year)).to_json())  # returning a string in the http response
+
+
+@app.get('/indicator_avg/<indicator_code>/<year>')  # defining a get endpoint
+def average(indicator_code, year):
+    return get_indicator_average(indicator_code, int(year))  # returning a string in the http response
